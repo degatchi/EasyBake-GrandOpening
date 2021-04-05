d6941d33533529a6be26a8429c1ee818114b76ca
DeGatchi <DeGatchi@users.noreply.github.com>
2021-04-06 00:54:43 +1000

    create-react-app boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar.js'
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar title="EasyBake Grand Opening!"/>
+    </div>
+  );
+}
+
+export default App;
