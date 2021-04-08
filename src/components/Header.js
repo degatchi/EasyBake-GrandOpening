4ea651242960c92b0d8def978a8232206f80c0b2
DeGatchi <DeGatchi@users.noreply.github.com>
2021-04-08 15:23:44 +1000

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {    
+    return (
+        <nav className='main-header'>
+            <h1>
+                <h4 className="header"> EasyBakeSwap</h4>
+            </h1>
+        </nav>
+    )
+}
+
+export default Header
