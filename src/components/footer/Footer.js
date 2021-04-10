ff4e55334c8dc0866c81b36b4ee311cfa5a4fb06
DeGatchi <DeGatchi@users.noreply.github.com>
2021-04-10 15:03:21 +1000

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,48 +1,74 @@
-import "./Footer.css";
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className="main-footer">
-      <div className="container">
-        <div className="row">
+    <div className='main-footer'>
+      <div className='container'>
+        <div className='row'>
           {/* Column1 */}
-          <div className="col">
+          <div className='col'>
             <h4>Contact</h4>
-            <ui className="list-unstyled">
+            <ui className='list-unstyled'>
               <li>EasyBakeSwap@gmail.com</li>
             </ui>
           </div>
           {/* Column2 */}
-          <div className="col">
+          <div className='col'>
             <h4>Socials</h4>
-            <ui className="list-unstyled">
-              <a href="https://twitter.com/EasyBakeSwap" target="_blank">Twitter</a>
-              <br/>
-              <a href="https://t.me/EasyBakeSwap" target="_blank">Telegram</a>
-              <br/>
-              <a href="https://github.com/EasybakeSwap" target="_blank">Github</a>
+            <ui className='list-unstyled'>
+              <a href='https://twitter.com/EasyBakeSwap' target='_blank'>
+                Twitter
+              </a>
+              <br />
+              <a href='https://t.me/EasyBakeSwap' target='_blank'>
+                Telegram
+              </a>
+              <br />
+              <a href='https://github.com/EasybakeSwap' target='_blank'>
+                Github
+              </a>
             </ui>
           </div>
           {/* Column3 */}
-          <div className="col">
+          <div className='col'>
             <h4>Contract Addresses</h4>
-            <ui className="list-unstyled">
-              <a href="https://ropsten.etherscan.io/address/0xce2ac9a13c4ec3bc2979e34d91ba088afbf2caf4" target="_blank">OvenToken.sol</a>
-              <br/>
-              <a href="https://ropsten.etherscan.io/address/0x08790cd613eedc74d1bf01acab4a8dcbbfe44e92" target="_blank">SugarBar.sol</a>
+            <ui className='list-unstyled'>
+              <a
+                href='https://ropsten.etherscan.io/address/0x1acb479bb9d1f73009f85ef5f495e942bb57f15a'
+                target='_blank'
+              >
+                OvenToken.sol
+              </a>
+              <br />
+              <a
+                href='https://ropsten.etherscan.io/address/0x11058ea62665ac8ab5cae5a2abc97e5451ace604#code'
+                target='_blank'
+              >
+                SugarBar.sol
+              </a>
+              <br />
+              <a
+                href='https://ropsten.etherscan.io/address/0xc6deeacf599d97761cd03ce0aac45964daebc234#code'
+                target='_blank'
+              >
+                MasterChef.sol
+              </a>
             </ui>
           </div>
         </div>
         <hr />
-        <div className="row">
-          <p className="col-sm">
-            &copy;{new Date().getFullYear()} <a href="https://easybake.finance/" target="_blank">EasyBakeSwap</a> | All rights reserved |
-            Terms Of Service | Privacy
+        <div className='row'>
+          <p className='col-sm'>
+            &copy;{new Date().getFullYear()}{' '}
+            <a href='https://easybake.finance/' target='_blank'>
+              EasyBakeSwap
+            </a>{' '}
+            | All rights reserved | Terms Of Service | Privacy
           </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Footer;
