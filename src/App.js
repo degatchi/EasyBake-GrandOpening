07cd9a5927eb3edac64a412431dde89c7e6a9e1d
DeGatchi <DeGatchi@users.noreply.github.com>
2021-04-13 23:01:49 +1000

    patch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import React, { useEffect, useState } from 'react';
-// import { BigNumber, ethers } from 'ethers';
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
+
 import Footer from './components/footer/Footer.js';
 import Header from './components/header/Header.js';
-import getBlockchain from './components/blockchain/getBlockchain.js';
-// import Web3Modal from 'web3modal';
+import MasterChefABI from './abis/MasterChefABI.json';
+import EthDaiABI from './abis/EthDaiABI.json';
+
 import './App.css';
 
 const App = ({ depositAmount, withdrawAmount, poolId }) => {
+  const [connected, setConnection] = useState(false);
   const [account, setAccount] = useState(undefined);
   const [_pendingOven, setPendingOven] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
   const [MasterChef, setMasterChef] = useState(undefined);
+  const [EthDaiLPT, setEthDaiLPT] = useState(undefined);
 
   useEffect(() => {
     const init = async () => {
-      getBlockchain();
+      if (typeof window.ethereum !== 'undefined') {
+        setConnection(true);
+        let web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
+        const netId = await web3.eth.net.getId();
+        const accounts = await web3.eth.getAccounts();
+
+        // load balance
+        if (typeof accounts[0] !== 'undefined') {
+          const balance = await web3.eth.getBalance(accounts[0]);
+          setAccount(accounts[0]);
+          setBalance(balance);
+          web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
+        } else {
+          window.ethereum.enable();
+          window.alert('Please login & refresh');
+        }
+
+        // sets contract(s)
+        try {
+          setMasterChef(
+            new web3.eth.Contract(
+              MasterChefABI,
+              '0xc6deeacf599d97761cd03ce0aac45964daebc234'
+            )
+          );
+          setEthDaiLPT(
+            new web3.eth.Contract(
+              EthDaiABI,
+              '0x1c5DEe94a34D795f9EEeF830B68B80e44868d316'
+            )
+          );
+        } catch (e) {
+          window.alert(
+            `Contracts not deployed to the current network: ${netId}`
+          );
+        }
+      }
     };
     init();
-  });
+  }, []);
+
+  const approveETHDAI = async (amount) => {
+    await EthDaiLPT.methods
+      .approve('0xc6deeacf599d97761cd03ce0aac45964daebc234', amount)
+      .send({ from: account });
+  };
+
+  const allowanceETHDAI = async () => {
+    const allowance = await EthDaiLPT.methods
+      .allowance(MasterChef, account)
+      .call();
+  };
 
   const depositETHDAI = async (amount) => {
+    await approveETHDAI(amount);
+    // window.ethereum.enable();
     await MasterChef.methods.deposit('2', amount).send({ from: account });
   };
 
@@ -30,12 +85,10 @@ const App = ({ depositAmount, withdrawAmount, poolId }) => {
   // [ ] Need to fix
   const pendingOven = async (poolId) => {
     try {
-      await MasterChef.methods
-        .pendingOven(poolId, account)
-        .call()
-        .on('confirmation', function (receipt) {
-          setPendingOven(receipt);
-        });
+      const returnValue = await MasterChef.methods
+        .pendingOven(poolId.toString(), account)
+        .call();
+      setPendingOven(returnValue);
     } catch (err) {
       window.alert(err);
     }
@@ -47,7 +100,7 @@ const App = ({ depositAmount, withdrawAmount, poolId }) => {
       <div className='content-wrap'>
         <Header />
         <br />
-        {/* <button type='submit' onClick={window.ethereum.enable()}>
+        {/* <button type='submit' onClick={connect()}>
           Connect Wallet
         </button> */}
         <hr />
